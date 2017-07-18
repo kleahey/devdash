b16b8e1e5106abde0dc43887b6862b8106e363d6
Kevin Leahey <kleahey@me.com>
2017-07-18 12:03:20 -0400

    added simple form for rails forms and ability to upload photo to create meme post

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -69,6 +69,6 @@ class MemesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def meme_params
-      params.require(:meme).permit(:caption)
+      params.require(:meme).permit(:caption, :image)
     end
 end
