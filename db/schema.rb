6ba6e2d323671fcd6f31a292f5b0cd5fd43aecfb
Kevin Leahey <kleahey@me.com>
2017-07-19 13:32:56 -0400

    bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170718194021) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "iterations", force: :cascade do |t|
     t.string "object_id"
     t.datetime "end_date"
