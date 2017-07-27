84faa77b826a391d86cb2b94366ebb399b390526
Kevin Leahey <kleahey@me.com>
2017-07-27 12:50:17 -0400

    DD-16 Project table created and migrated to db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727163017) do
+ActiveRecord::Schema.define(version: 20170727164904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20170727163017) do
   end
 
   create_table "programs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.integer "assigned_id"
+    t.integer "program_id"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
