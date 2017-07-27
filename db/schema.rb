1bbcecc99ef71e27a4977fb4df5d1061de132219
Kevin Leahey <kleahey@me.com>
2017-07-27 13:18:52 -0400

    DD-19 updated the seed file to include information about the project table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727164904) do
+ActiveRecord::Schema.define(version: 20170727170820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170727164904) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.integer "assigned_id"
+    t.bigint "assigned_id"
     t.integer "program_id"
     t.string "name"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,17 @@ program_list.each do |x|
   record = Program.find_or_initialize_by(name: x)
   record.save!
 end
+
+project_list = [
+  [ 41877373746, "CA Web App", 1 ],
+  [ 49069534069, "TCC", 1 ],
+  [ 36762812213, "onTrack", 1 ],
+  [ 130195603136, "CA Analytics", 1 ]
+]
+
+project_list.each do |x, y, z|
+  record = Project.find_or_initialize_by(assigned_id: x)
+  record.name = y
+  record.program_id = z
+  record.save!
+end
