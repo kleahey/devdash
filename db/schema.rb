a66694605da473efe36ed837fd5548a7a52ea2ab
Kevin Leahey <kleahey@me.com>
2017-07-27 13:23:17 -0400

    DD-15 removed existing Tasks table from a prior merge to clear the way for new table

--- db/schema.rb
+++ db/schema.rb
@@ -61,13 +61,4 @@ ActiveRecord::Schema.define(version: 20170727170820) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.datetime "historical_date"
-    t.string "project"
-    t.float "todo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "unformatted_id"
-  end
-
 end
