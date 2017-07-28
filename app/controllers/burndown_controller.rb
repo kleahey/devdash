ac66d41c89c8f07fb0dbca771f656beb796e54f0
Kevin Leahey <kleahey@me.com>
2017-07-28 15:23:58 -0400

    DD-28 Burndown controller and view updated to pull data from the Tasks table in the DB

--- app/controllers/burndown_controller.rb
+++ app/controllers/burndown_controller.rb
@@ -1,5 +1,11 @@
 class BurndownController < ApplicationController
   def index
+    @title = Sprint.all
+    @dates = Task.select("date(historical_date) as date").order("date(historical_date)").group("date(historical_date)")
+    @webapp = Task.select("task_hours as hours").where("project_id = 41877373746")
+    @tcc = Task.select("task_hours as hours").where("project_id = 49069534069")
+    @ontrack = Task.select("task_hours as hours").where("project_id = 36762812213")
+    @analytics = Task.select("task_hours as hours").where("project_id = 130195603136")
     if params[:search].blank?
       render 'search'
     else
