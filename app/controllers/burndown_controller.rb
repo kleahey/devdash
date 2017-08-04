3a711b6f9be88c539dbd358fdd50e2e8c10844fb
Kevin Leahey <kleahey@me.com>
2017-08-04 11:56:50 -0400

    DD-35 charts now pull information dynamically based on form input

--- app/controllers/burndown_controller.rb
+++ app/controllers/burndown_controller.rb
@@ -2,11 +2,11 @@ class BurndownController < ApplicationController
 
   def index
     @title = Sprint.find_by_id(params[:sprint])
-    @dates = Task.select("date(historical_date) as date").order("date(historical_date)").group("date(historical_date)")
-    @webapp = Task.select("task_hours as hours").where("project_id = 41877373746")
-    @tcc = Task.select("task_hours as hours").where("project_id = 49069534069")
-    @ontrack = Task.select("task_hours as hours").where("project_id = 36762812213")
-    @analytics = Task.select("task_hours as hours").where("project_id = 130195603136")
+    @dates = Task.select("date(historical_date) as date").where(sprint_id: params[:sprint]).order("date(historical_date)").group("date(historical_date)")
+    @webapp = Task.select("task_hours as hours").where(sprint_id: params[:sprint], project_id: 41877373746)
+    @tcc = Task.select("task_hours as hours").where(sprint_id: params[:sprint], project_id: 49069534069)
+    @ontrack = Task.select("task_hours as hours").where(sprint_id: params[:sprint], project_id: 36762812213)
+    @analytics = Task.select("task_hours as hours").where(sprint_id: params[:sprint], project_id: 130195603136)
     if params[:search].blank?
       render 'search'
     else
