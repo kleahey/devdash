424d601fa2422e82a55c2c8e68d2a31a65ad7a8a
Kevin Leahey <kleahey@me.com>
2017-08-04 10:57:15 -0400

    DD-34 sprint_id added to Task table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727172618) do
+ActiveRecord::Schema.define(version: 20170804145306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20170727172618) do
     t.float "task_hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sprint_id"
   end
 
 end
