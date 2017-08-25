e048dab6ee37668bb0012b3b9bd8f3e986f609d2
Kevin Leahey <kleahey@me.com>
2017-08-25 15:27:52 -0400

    google maps plugin added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,6 +37,8 @@
 //= require fancybox/jquery.fancybox.pack.js
 //= require fullcalendar/fullcalendar.js
 //= require fullcalendar/gcal.js
+//= require gmaps/gmaps.js
+//= require gmaps/google_maps_api.js
 //= require data-tables/DT_bootstrap.js
 //= require data-tables/jquery.dataTables.js
 //= require datatables-responsive/datatables.responsive.js
