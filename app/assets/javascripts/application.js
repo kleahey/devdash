dae941f7d95bcacd0d3899ee1985cf2a80327397
Kevin Leahey <kleahey@me.com>
2017-08-25 15:22:07 -0400

    fancybox plugin added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,6 +34,7 @@
 //= require bootstrap-select2/select2.js
 //= require breakpoints/breakpoints.js
 //= require clockface/clockface.js
+//= require fancybox/jquery.fancybox.pack.js
 //= require data-tables/DT_bootstrap.js
 //= require data-tables/jquery.dataTables.js
 //= require datatables-responsive/datatables.responsive.js
