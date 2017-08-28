c077aebb34445ec8c0e12471666ffb1a81112989
Kevin Leahey <kleahey@me.com>
2017-08-28 13:34:46 -0400

    jquery-unveil plugin added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,6 +27,7 @@
 //= require jquery-polymaps/polymaps.min.js
 //= require jquery-lazyload/jquery.lazyload.min.js
 //= require jquery-knob/jquery.knob.js
+//= require jquery-unveil/jquery.unveil.min.js
 //= require owl-carousel/owl.carousel.js
 //= require mapplic/jquery.easing.js
 //= require mapplic/jquery.mousewheel.js
